694f5e44d930805a0dc89fe865529ee3bb63f8f9
adarshvp25 <adarshvardhanpatel00@gmail.com>
2026-08-04 13:08:10 +0530

    feat(analytics): integrate Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import { siteConfig, ogImage } from "@/lib/site";
 import { Navbar } from "@/components/layout/navbar/navbar";
@@ -54,6 +55,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-X85RQHHX94" />
     </html>
   );
 }
